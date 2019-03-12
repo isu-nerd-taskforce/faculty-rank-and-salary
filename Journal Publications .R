6cc8a54cfa0daa3caac6a735327056a0601dd6a0
Fuller <samf1021@iastate.edu>
2019-03-12 16:05:25 -0500

    SCrape top journals for political science

--- Journal Publications .R
+++ Journal Publications .R
@@ -6,24 +6,6 @@ myurl <- ("https://scholar.google.com/citations?view_op=top_venues&hl=en&vq=soc_
 myhtml<-read_html(myurl)
 
 mynodes<-html_nodes(myhtml,"td")
-
-myattr<-html_attr(mynodes, "class")
-myattr
-#Alternatively, use the piping operator 
-facpaths<-html_nodes(myhtml, "a")[62:86] %>% html_attr("href")
-Please work
-#Stumping the constant for href e.g. header 
-mystub<-"https://www.pols.iastate.edu"
-myurls<-paste0(mystub, myattr)
-#why god, why
-#cbinding the function 
-get_info<-function(myurls){
-  myurls[1] %>% read_html() %>% html_nodes(".entry-title") %>% html_text() -> pname
-  myurls[1] %>% read_html() %>% html_nodes(".title") %>% html_text() -> title
-  cbind.data.frame(pname,title)
-}
-ndsbfmndbfmn
-#To format the information (Usually rbind)
-lapply(myurls,get_info)
-#To clean up the information
-do.call(rbind, lapply(myurls, get_info))
+tex<-as.list(html_text(mynodes))
+tex<-gsub("[0-9]",NA,tex)
+tex<-as.list(na.omit(tex))
